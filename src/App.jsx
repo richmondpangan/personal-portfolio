d6b0d7364527f3a16c62943c8a65051acad7f1cc
Richmond <130723287+richmondpangan@users.noreply.github.com>
2024-03-31 13:34:50 +0800

    Added index.css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import "./index.css";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
+import Projects from "./components/projects/Projects";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
-      <h1>Blank Page</h1>
+      <Header />
+      <Nav />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
   )
 }
