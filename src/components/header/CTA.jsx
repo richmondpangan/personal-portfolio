568badc99d5229ccf1b2fba9ab8b551f8f88f9ee
Richmond <130723287+richmondpangan@users.noreply.github.com>
2024-04-02 21:02:48 +0800

    Modified CTA.jsx
    - Removed the download button

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import CV from '../../assets/Richmond-CV.pdf';
 
 function CTA() {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        <a href="#contact" className='btn btn-primary'>Contact Me</a>
     </div>
   )
 }
