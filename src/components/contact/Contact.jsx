902927f2d520c168928ba5dc7c928e4ca104de3f
Richmond <130723287+richmondpangan@users.noreply.github.com>
2024-04-02 21:12:51 +0800

    Added content to both Contact.jsx and contact.css files

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import "./contact.css"
+import { FaLinkedin } from "react-icons/fa";
+import { MdOutlineMailOutline } from "react-icons/md";
 
 function Contact() {
   return (
     <section id='contact'>
-      Contact
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__info-upper-container">
+        <div className="contact__info-container">
+          <MdOutlineMailOutline className='contact__icon' />
+          <p><a href="mailto:panganrichmond@gmail.com">panganrichmond@gmail.com</a></p>
+        </div>
+        <div className="contact__info-container">
+          <FaLinkedin className='contact__icon' />
+          <p><a href="https://www.linkedin.com/in/richmondpangan" target='_blank'>linkedin.com/in/richmondpangan</a></p>
+        </div>
+      </div>
     </section>
   )
 }
