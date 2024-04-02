ddc2704ac6690c346ad60b78a00dabd904213c37
Richmond <130723287+richmondpangan@users.noreply.github.com>
2024-04-02 21:02:00 +0800

    Modified Header.jsx
    - Added an image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import "./header.css";
 import CTA from './CTA';
+import IMG from '../../assets/profile.png';
 
 function Header() {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Mond</h1>
-        <h5 className='text-light'>Aspiring Software Engineer</h5>
-        <CTA />
+        
+        <div className="header__content">
+          <h5 className='greeting'>Hello, I'm</h5>
+          <h1>Richmond Pangan</h1>
+          <h5>Computer Science Graduate | Aspiring Software Engineer</h5>
+          <CTA />
+        </div>
+
+        <div className="header__me">
+          <div className="header__me-image">
+            <img src={IMG} alt="About Image" />
+          </div>
+        </div>
+
       </div>
     </header>
   )
