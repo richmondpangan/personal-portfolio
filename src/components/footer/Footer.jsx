8cf296d56bf4ce9d9be1f900724d86b0ec0bf25e
Richmond <130723287+richmondpangan@users.noreply.github.com>
2024-04-02 21:13:58 +0800

    Removed footer socials

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import "./footer.css";
-import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 function Footer() {
   return (
     <footer>
-      <a href="#" className='footer__logo'>LOGO</a>
+      <a href="#" className='footer__logo'>MOND</a>
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
@@ -14,13 +13,8 @@ function Footer() {
         <li><a href="#contact">Contact</a></li>
       </ul>
 
-      <div className="footer__socials">
-        <a href="https://www.linkedin.com/login" target='_blank'><FaLinkedin /></a>
-        <a href="https://github.com/" target='_blank'><FaGithub /></a>
-      </div>
-
       <div className="footer__copyright">
-        <small>&copy; LOGO. All rights reserved.</small>
+        <small>&copy; MOND. All rights reserved.</small>
       </div>
     </footer>
   )
